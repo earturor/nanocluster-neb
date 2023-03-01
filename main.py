aada4da6b6c866477e2f6f128634a694dce95b36
earturor <80609721+earturor@users.noreply.github.com>
2023-03-01 12:55:45 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+# This is a program to optimize Nanoclusters
+# Arturo Rentería
+# email: earturordr@outlook.com
+
+from argparse import ArgumentParser
+from time import perf_counter
+
+from potentials.gupta import GuptaPotential
+
+
+def main():
+    parser = ArgumentParser("Generate optimised nano-clusters")
+    parser.add_argument("epochs", type=int)
+    parser.add_argument("--fe", type=int, default=0)
+    parser.add_argument("--co", type=int, default=0)
+    parser.add_argument("--ni", type=int, default=0)
+    parser.add_argument("--pd", type=int, default=0)
+    parser.add_argument("--pt", type=int, default=0)
+    args = parser.parse_args()
+
+    for i in range(args.epochs):
+        atoms = {
+            "Fe": args.fe,
+            "Co": args.co,
+            "Ni": args.ni,
+            "Pd": args.pd,
+            "Pt": args.pt,
+        }
+
+        path_src = f"initial-{i:03}.xyz"
+        path_out = f"optim-{i:03}.xyz"
+
+        gp, c1 = GuptaPotential.random(atoms)
+        e1 = gp.potential(c1)
+
+        gp.write_xyz_file(path_src, c1)
+
+        t0 = perf_counter()
+        e2, c2 = gp.optimize(c1)
+        dt = perf_counter() - t0
+
+        gp.write_xyz_file(path_out, c2)
+
+        print(f"{e1:.2f},{e2:.2f},{path_src},{path_out},{dt:.3f}")
+
+
+if __name__ == "__main__":
+    main()
